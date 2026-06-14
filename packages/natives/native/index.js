971fd6d15cce107012e8cb199439a1256b41413b
luk <16418011+DeprecatedLuke@users.noreply.github.com>
2026-06-14 05:05:44 +0100

    fix(pi-natives): avoid deadlock by deferring tokio runtime install
    (cherry picked from commit c1a1cb6149e73b345919dd4cf629b0d9ac74fb57)

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -23,6 +23,7 @@ export const PtySession = nativeBindings.PtySession;
 export const Shell = nativeBindings.Shell;
 
 // functions
+export const __ompInstallTokioRuntime = nativeBindings.__ompInstallTokioRuntime;
 export const __piNativesV15_12_6 = nativeBindings.__piNativesV15_12_6;
 export const applyBashFixups = nativeBindings.applyBashFixups;
 export const astEdit = nativeBindings.astEdit;
